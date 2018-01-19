54b9fd5f4327f6ef49d81ce5ef5b4ce9a2f607d6
Loic Denuziere <loic@denuziere.net>
2018-01-19 16:10:56 +0100

    Convert to .NET Standard

--- build.fsx
+++ build.fsx
@@ -1,28 +1,17 @@
-#load "tools/includes.fsx"
-open IntelliFactory.Build
+#load "paket-files/build/intellifactory/websharper/tools/WebSharper.Fake.fsx"
+open Fake
+open WebSharper.Fake
 
-let buildTool =
-    BuildTool().PackageId("WebSharper.ChartJs")
-        .VersionFrom("WebSharper", versionSpec = "(,4.0)")
-        .WithFSharpVersion(FSharpVersion.FSharp30)
-        .WithFramework(fun f -> f.Net40)
+let targets =
+    GetSemVerOf "WebSharper"
+    |> ComputeVersion
+    |> WSTargets.Default
+    |> MakeTargets
 
-let main =
-    buildTool.WebSharper.Extension("WebSharper.ChartJs")
-        .SourcesFromProject()
-        .Embed(["Chart.min.js"])
+Target "Build" DoNothing
+targets.BuildDebug ==> "Build"
 
-buildTool.Solution [
-    main
-    buildTool.NuGet.CreatePackage()
-        .Configure(fun configuration ->
-            { configuration with
-                Title = Some "WebSharper.ChartJs"
-                LicenseUrl = Some "http://websharper.com/licensing"
-                ProjectUrl = Some "https://bitbucket.org/IntelliFactory/websharper.chartjs"
-                Description = "WebSharper Extension for Chart.js"
-                Authors = ["IntelliFactory"]
-                RequiresLicenseAcceptance = true })
-        .Add(main)
-]
-|> buildTool.Dispatch
+Target "CI-Release" DoNothing
+targets.CommitPublish ==> "CI-Release"
+
+RunTargetOrDefault "Build"
