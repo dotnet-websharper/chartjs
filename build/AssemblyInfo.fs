cd6a3dcade40ba38e6adce3fb77bb2f27e5b99d5
WebSharper CI <ci@websharper.com>
2018-08-29 09:07:51 -0700

    Version 4.5.0.138

--- build/AssemblyInfo.fs
+++ build/AssemblyInfo.fs
@@ -3,9 +3,9 @@ namespace System
 open System.Reflection
 
 [<assembly: AssemblyVersionAttribute("4.5.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("4.5.0.137")>]
+[<assembly: AssemblyFileVersionAttribute("4.5.0.138")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyVersion = "4.5.0.0"
-    let [<Literal>] AssemblyFileVersion = "4.5.0.137"
+    let [<Literal>] AssemblyFileVersion = "4.5.0.138"
