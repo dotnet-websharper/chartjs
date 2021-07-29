05417c29272c2dbfe15b4d26699882c1b313eb2b
Jooseppi12 <Jooseppi12@users.noreply.github.com>
2021-07-29 17:55:40 +0000

    deploy: 3406cbcc67f06bcd7d2cd68eb551c7e2068e591b

---
+++ Content/WebSharper.ChartJs.Testing.head.js
@@ -0,0 +1,1 @@
+document.write("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.4.1/chart.min.js\" type=\"text/javascript\" charset=\"UTF-8\"></script><!--[if lte IE 11.0]>\r\n<![endif]-->\r\n<!--[if lte IE 9.0]>\r\n<![endif]-->\r\n")
