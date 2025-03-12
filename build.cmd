aa9c19c040cc2df8d29ca3e0c2737e88bbf485a0
András Jankó <andras.janko@gmail.com>
2025-03-12 10:40:27 +0100

    Update build.cmd fix -t

--- build.cmd
+++ build.cmd
@@ -6,4 +6,11 @@ dotnet tool restore
 dotnet paket update -g wsbuild --no-install
 if errorlevel 1 exit /b %errorlevel%
 
-dotnet fsi ./build.fsx -t %*
+set _Add-t=""
+set FirstArg=%1
+if not "%FirstArg%"=="" if not "%FirstArg:~0,1%"=="-" set _Add-t=1
+if "%_Add-t%"=="1" (
+  dotnet fsi ./build.fsx -t %*
+) else (
+  dotnet fsi ./build.fsx %*
+)
