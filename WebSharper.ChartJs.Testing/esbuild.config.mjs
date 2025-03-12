d676f601de2d0b9ca228ef14b5703d2cc2b642a9
András Jankó <andras.janko@gmail.com>
2025-03-12 15:38:46 +0100

    updates for WS8

---
+++ WebSharper.ChartJs.Testing/esbuild.config.mjs
@@ -0,0 +1,14 @@
+﻿import { build } from 'esbuild'
+
+var options =
+{
+  entryPoints: ['./build/WebSharper.ChartJs.Testing.js'],
+  bundle: true,
+  minify: true,
+  format: 'iife',
+  outfile: '../dist/Content/WebSharper.ChartJs.Testing.min.js',
+  globalName: 'wsbundle'
+};
+
+build(options);
+
